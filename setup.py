a081980e4282620ada507e215978c2b7420148a3
Yusheng Cai <cys9741@seas.upenn.edu>
2021-03-25 00:59:02 -0400

    Changed setup.py to include the droplet code

--- setup.py
+++ setup.py
@@ -19,6 +19,9 @@ extensions = [Extension("analysis_code.md",["analysis_code/md.pyx"],\
             define_macros=[("NPY_NO_DEPRECATED_API", "NPY_1_7_API_VERSION")]),\
         Extension("analysis_code.Liquid_crystal.GAFF_LC",["analysis_code/Liquid_crystal/GAFF_LC.pyx"],\
             include_dirs=[numpy.get_include()], \
+            define_macros=[("NPY_NO_DEPRECATED_API", "NPY_1_7_API_VERSION")]),\
+        Extension("analysis_code.droplet.droplet",["analysis_code/droplet/droplet.pyx"],\
+            include_dirs=[numpy.get_include()], \
             define_macros=[("NPY_NO_DEPRECATED_API", "NPY_1_7_API_VERSION")])]
 
 setup(name='analysis_code',ext_modules = cythonize(extensions,compiler_directives={'language_level' : "3"}))
