8a587ddc52bbfa7d4db33e889aa49517e363d474
Yusheng Cai <cys9741@seas.upenn.edu>
2021-06-03 20:17:13 -0400

    Updated 2 tests, INDUS & p2 variants

--- analysis_code/test/test_indus.py
+++ analysis_code/test/test_indus.py
@@ -1,16 +1,60 @@
-import sys
 import numpy as np
-sys.path.insert(0,"../Liquid_crystal/")
-from INDUS_util import *
+import sys
+
+sys.path.insert(0,"../Liquid_crystal")
+sys.path.insert(0,"../ProbeVolume")
+from analysis_code.Liquid_crystal.nCB import nCB_PV
+from analysis_code.ProbeVolume.ProbeVolume_Box import ProbeVolume_Box
+
+def make_reference():
+   min_ = np.array([10,20,0])
+   max_ = np.array([20,40,30])
+   pv = ProbeVolume_Box("5cb_traj/run.tpr","5cb_traj/traj_comp.xtc",min_,max_)
+   LC_pv = nCB_PV("5cb_traj/run.tpr","5cb_traj/traj_comp.xtc", 5, pv,trjconv=False) 
+   Ntilde_ref    = np.zeros((len(LC_pv),1))
+   indicator_ref = np.zeros((len(LC_pv),LC_pv.Nresidues_,1)) 
+   hx_ref        = np.zeros((len(LC_pv),LC_pv.Nresidues_,3))
+
+   for i in range(len(LC_pv)):
+       head_pos , _  = LC_pv.head_tail_pos(i)
+       indicator, hx = pv.calculate_Indicator(head_pos,i)
+       indicator_ref[i] = indicator
+       hx_ref[i] = hx
+       Ntilde_ref[i] = pv.get_Ntilde()
+    
+   np.save("INDUS_test/indicator_ref.npy", indicator_ref)
+   np.save("INDUS_test/hx_ref.npy",hx_ref)
+   np.save("INDUS_test/Ntilde_ref.npy", Ntilde_ref)
 
 def test_INDUS():
-    pos = np.load("INDUS_test/random_pos_for_indus.npy")
-    min_= np.array([-1.2,-1.2,-1.2])
-    max_= -min_
+   min_ = np.array([10,20,0])
+   max_ = np.array([20,40,30])
+   pv = ProbeVolume_Box("5cb_traj/run.tpr","5cb_traj/traj_comp.xtc",min_,max_)
+   LC_pv = nCB_PV("5cb_traj/run.tpr","5cb_traj/traj_comp.xtc", 5, pv,trjconv=False) 
+   Ntilde    = np.zeros((len(LC_pv),1))
+   indicator = np.zeros((len(LC_pv),LC_pv.Nresidues_,1)) 
+   hx_     = np.zeros((len(LC_pv),LC_pv.Nresidues_,3))
 
-    kk=h(pos,min_,max_)
-    ans = np.load("INDUS_test/indus_answer.npy")
-    assert(np.max(np.abs(ans-kk)) < 1e-10)
+   for i in range(len(LC_pv)):
+       head_pos , _  = LC_pv.head_tail_pos(i)
+       ind, hx = pv.calculate_Indicator(head_pos,i)
+       indicator[i] = ind
+       hx_[i] = hx
+       Ntilde[i] = pv.get_Ntilde()
+    
+   ans_Ntilde       = np.load("INDUS_test/Ntilde_ref.npy")
+   ans_indicator    = np.load("INDUS_test/indicator_ref.npy")
+   ans_hx           = np.load("INDUS_test/hx_ref.npy")
+
+   err1 = np.max(np.abs(ans_Ntilde - Ntilde))
+   err2 = np.max(np.abs(ans_indicator - indicator))
+   err3 = np.max(np.abs(ans_hx - hx_))
+
+   assert(err1 < 1e-10)
+   assert(err2 < 1e-10)
+   assert(err3 < 1e-10)
+
 
 if __name__ == "__main__":
-   test_INDUS()
+    #make_reference()
+    test_INDUS()
--- analysis_code/test/test_p2variants.py
+++ analysis_code/test/test_p2variants.py
@@ -1,81 +1,61 @@
 import sys
-import matplotlib.pyplot as plt
 import numpy as np
 sys.path.insert(0,"../Liquid_crystal")
-from Liquid_crystal import *
+from analysis_code.Liquid_crystal.nCB import nCB, nCB_PV
+from analysis_code.ProbeVolume.ProbeVolume_Box import ProbeVolume_Box
 
+min_  = np.array([10.0,20.0,0.0])
+max_  = np.array([20.0,40.0,30.0])
+n  = np.array([1,2,3])
 
-def make_p2_ref():
-    LC = nCB("5cb_traj/run.tpr","5cb_traj/traj_comp.xtc", 5, 1, trjconv=False)
+def make_ref():
+    LC      = nCB("5cb_traj/run.tpr","5cb_traj/traj_comp.xtc",5,trjconv=False)
+    pv_     = ProbeVolume_Box("5cb_traj/run.tpr","5cb_traj/traj_comp.xtc",min_,max_)
+    LC_tilde= nCB_PV("5cb_traj/run.tpr","5cb_traj/traj_comp.xtc",5,pv_,trjconv=False)
+
     p2_list = np.zeros((len(LC),))
+    p2tilde_list = np.zeros((len(LC),))
+    p2cos_list = np.zeros((len(LC),))
 
     for i in range(len(LC)):
         p2_list[i] = LC.p2(i)
+        p2tilde_list[i] = LC_tilde.p2tilde(i)
+        p2cos_list[i] = LC.p2_cos(i,n)
+
+        print("p2 at {} is {:.5f}".format(i, p2_list[i]))
+        print("p2tilde at {} is {:.5f}".format(i, p2tilde_list[i]))
+        print("p2cos at {} is {:.5f}".format(i, p2cos_list[i]))
 
     np.save("5cb_traj/p2_ref.npy",p2_list)
-
-def make_p2tilde_ref():
-    LC = nCB("5cb_traj/run.tpr","5cb_traj/traj_comp.xtc", 5, 1, trjconv=False)
-    p2tilde_list = np.zeros((len(LC),))
-    min_  = np.array([10.0,20.0,0.0])
-    max_  = np.array([20.0,40.0,30.0])
-
-    for i in range(len(LC)):
-        p2tilde_list[i] = LC.p2tilde(i, min_, max_)
-        print(p2tilde_list[i])
-
     np.save("5cb_traj/p2tilde_ref.npy",p2tilde_list)
-
-def make_p2cos_ref():
-    LC = nCB("5cb_traj/run.tpr","5cb_traj/traj_comp.xtc", 5, 1, trjconv=False)
-    p2cos_list = np.zeros((len(LC),))
-    n  = np.array([1,2,3])
-
-    for i in range(len(LC)):
-        p2cos_list[i] = LC.p2_cos(i,n)
-        print(p2cos_list[i])
-
     np.save("5cb_traj/p2cos_ref.npy",p2cos_list)
 
 def test_p2():
-    LC = nCB("5cb_traj/run.tpr","5cb_traj/traj_comp.xtc", 5, 1, trjconv=False)
+    LC      = nCB("5cb_traj/run.tpr","5cb_traj/traj_comp.xtc",5,trjconv=False)
+    pv_     = ProbeVolume_Box("5cb_traj/run.tpr","5cb_traj/traj_comp.xtc",min_,max_)
+    LC_tilde= nCB_PV("5cb_traj/run.tpr","5cb_traj/traj_comp.xtc",5,pv_,trjconv=False)
+
     p2_list = np.zeros((len(LC),))
+    p2tilde_list = np.zeros((len(LC),))
+    p2cos_list = np.zeros((len(LC),))
 
     for i in range(len(LC)):
         p2_list[i] = LC.p2(i)
-
-    ref = np.load("5cb_traj/p2_ref.npy")
-    err = np.max(np.abs(ref-p2_list)/np.abs(ref))
-    assert err < 1e-10
-
-def test_p2tilde():
-    LC = nCB("5cb_traj/run.tpr","5cb_traj/traj_comp.xtc", 5, 1, trjconv=False)
-    p2tilde_list = np.zeros((len(LC),))
-    min_  = np.array([10.0,20.0,0.0])
-    max_  = np.array([20.0,40.0,30.0])
-
-    for i in range(len(LC)):
-        p2tilde_list[i] = LC.p2tilde(i, min_, max_)
-
-    ref = np.load("5cb_traj/p2tilde_ref.npy")
-    err = np.max(np.abs(ref-p2tilde_list)/np.abs(ref))
-    assert err < 1e-10
-
-def test_p2cos():
-    LC = nCB("5cb_traj/run.tpr","5cb_traj/traj_comp.xtc", 5, 1, trjconv=False)
-    p2cos_list = np.zeros((len(LC),))
-    n  = np.array([1,2,3])
-
-    for i in range(len(LC)):
+        p2tilde_list[i] = LC_tilde.p2tilde(i)
         p2cos_list[i] = LC.p2_cos(i,n)
 
-    ref = np.load("5cb_traj/p2cos_ref.npy")
-    err = np.max(np.abs(ref - p2cos_list)/np.abs(ref))
-    assert err < 1e-10
+    p2_ref = np.load("5cb_traj/p2_ref.npy")
+    p2tilde_ref = np.load("5cb_traj/p2tilde_ref.npy")
+    p2cos_ref   = np.load("5cb_traj/p2cos_ref.npy")
 
+    err1 = np.max(np.abs(p2_ref - p2_list)/np.abs(p2_ref))
+    err2 = np.max(np.abs(p2tilde_ref - p2tilde_list)/np.abs(p2tilde_ref))
+    err3 = np.max(np.abs(p2cos_ref - p2cos_list)/np.abs(p2cos_list))
 
+    assert(err1 < 1e-10)
+    assert(err2 < 1e-10)
+    assert(err3 < 1e-10)
 
 if __name__ == '__main__':
-    # make ref
+    #make_ref()
     test_p2()
-    test_p2tilde()
