65b82d88d55585a8a0511c36823af62b750df1ad
Yusheng Cai <cys9741@seas.upenn.edu>
2021-06-04 15:12:59 -0400

    Used indicator function object to take care of all the calculations

--- analysis_code/ProbeVolume/ProbeVolume_Box.py
+++ analysis_code/ProbeVolume/ProbeVolume_Box.py
@@ -4,6 +4,7 @@ import MDAnalysis as mda
 
 from .ProbeVolume import _ProbeVolume
 from .Bounding_box import Bounding_box
+from .indicator_func_2b import indicator_func_2b
 
 class ProbeVolume_Box(_ProbeVolume):
     """
@@ -22,13 +23,14 @@ class ProbeVolume_Box(_ProbeVolume):
         self.tpr    = tpr
         self.xtc    = xtc
         self.u_     = mda.Universe(tpr,xtc)
+        self.bounding_box  = Bounding_box(self.u_)
 
         # shift min_ & max_
         self.center_       = 1/2*(max_ + min_)
         self.max_shifted   = max_ - self.center_
         self.min_shifted   = min_ - self.center_
 
-        self.bounding_box  = Bounding_box(self.u_)
+        self.func_  = indicator_func_2b(self.min_shifted, self.max_shifted, sigma=sigma, ac=ac)
     
     def calculate_Indicator(self,pos:np.ndarray, ts:int):
         """
@@ -48,27 +50,10 @@ class ProbeVolume_Box(_ProbeVolume):
             indicator(numpy.ndarray)     : The indicator array in shape (N,1)
             hx(numpy.ndarray)            : The indicator array of each DIM (N,3)
         """
-        sigma  = self.sigma_
-        ac     = self.ac_
-        max_   = self.max_shifted
-        min_   = self.min_shifted
         bb     = self.bounding_box
 
         pos_center    = bb.dr_pbc(pos - self.center_, ts) 
-
-        # normalizing constants
-        k = np.sqrt(2*np.pi*sigma**2) * erf(ac/np.sqrt(2*sigma**2)) - 2*ac*np.exp(-ac**2/(2*sigma**2))
-        k1 = 1/k*np.sqrt(np.pi*sigma**2/2)
-        k2 = 1/k*np.exp(-ac**2/(2*sigma**2))
-
-        p1 = (k1*erf((max_ - pos_center)/(np.sqrt(2)*sigma)) - k2*(max_ - pos_center) - 1/2)*\
-        np.heaviside(ac - np.abs(max_ - pos_center), 1.0)
-
-        p2 = (k1*erf((pos_center - min_)/(np.sqrt(2)*sigma)) - k2*(pos_center - min_) - 1/2)*\
-        np.heaviside(ac - np.abs(pos_center - min_),1.0)
-
-        p3 = np.heaviside(ac + 1/2*(max_ - min_) - np.abs(pos_center - 1/2*(max_ + min_)),1.0)
-        hx = p1 + p2 + p3
+        hx            = self.func_.calculate(pos_center)
 
         indicator       = np.prod(hx,axis=1,keepdims=True)
         self.indicator_ = indicator
@@ -76,22 +61,6 @@ class ProbeVolume_Box(_ProbeVolume):
         self.hx_        = hx
 
         return indicator, hx
-
-    def phi(self, r):
-        """
-        Calculate phi function in INDUS where h = \int phi
-
-        Args:
-            r(numpy.ndarray)    : The alpha variable in the phi function for INDUS
-        """
-        sigma = self.sigma_
-        ac    = self.ac_
-
-        # Calculate the phi function
-        k = np.sqrt(2*np.pi*sigma**2) * erf(ac/np.sqrt(2*sigma**2)) - 2*ac*np.exp(-ac**2/(2*sigma**2))
-        p = 1/k*(np.exp(-r**2/(2*sigma**2)) - np.exp(-ac**2/(2*sigma**2)))*np.heaviside(ac - np.abs(r),1)
-
-        return p
 
     def calculate_derivative(self,pos,hx, ts):
         """
@@ -105,15 +74,15 @@ class ProbeVolume_Box(_ProbeVolume):
         Returns:
             derivative of hprime(numpy.ndarray) : ((N,3))
         """
-        max_    = self.max_shifted
-        min_    = self.min_shifted
-        center_ = self.center_
-        bb      = self.bounding_box
+        center_       = self.center_
+        bb            = self.bounding_box
         pos_center    = bb.dr_pbc(pos - center_, ts) 
 
-        deriv_x = -(self.phi(max_[0]-pos_center[:,0]) - self.phi(min_[0]-pos_center[:,0]))*hx[:,1]*hx[:,2]
-        deriv_y = -(self.phi(max_[1]-pos_center[:,1]) - self.phi(min_[1]-pos_center[:,1]))*hx[:,2]*hx[:,0]
-        deriv_z = -(self.phi(max_[2]-pos_center[:,2]) - self.phi(min_[2]-pos_center[:,2]))*hx[:,1]*hx[:,0]
+        hxprime       = self.func_.calculate_derivative(pos_center)
+
+        deriv_x       = hxprime[:,0]*hx[:,1]*hx[:,2]
+        deriv_y       = hx[:,0]*hxprime[:,1]*hx[:,2]
+        deriv_z       = hx[:,0]*hx[:,1]*hxprime[:,2]
 
         deriv           = np.hstack((deriv_x[:,np.newaxis],deriv_y[:,np.newaxis],deriv_z[:,np.newaxis]))
         self.derivative = deriv
