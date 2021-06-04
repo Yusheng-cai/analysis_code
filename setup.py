4615cee7a686f9a14ec64053d24a422ce5795660
Yusheng Cai <cys9741@seas.upenn.edu>
2021-06-03 20:16:25 -0400

    Updated setup.py such that we are no longer using cython

--- setup.py
+++ setup.py
@@ -1,24 +1,7 @@
-from setuptools import setup, Extension
-from Cython.Build import cythonize
-import numpy
+from setuptools import setup, find_packages
 
-extensions = [Extension("analysis_code.timeseries",["analysis_code/timeseries.pyx"],\
-            include_dirs=[numpy.get_include()], \
-            define_macros=[("NPY_NO_DEPRECATED_API", "NPY_1_7_API_VERSION")]),\
-        Extension("analysis_code.rdf",["analysis_code/rdf.pyx"],\
-            include_dirs=[numpy.get_include()], \
-            define_macros=[("NPY_NO_DEPRECATED_API", "NPY_1_7_API_VERSION")]), \
-        Extension("analysis_code.Liquid_crystal.Liquid_crystal",["analysis_code/Liquid_crystal/Liquid_crystal.pyx"],\
-            include_dirs=[numpy.get_include()], \
-            define_macros=[("NPY_NO_DEPRECATED_API", "NPY_1_7_API_VERSION")]), \
-        Extension("analysis_code.Liquid_crystal.INDUS_util",["analysis_code/Liquid_crystal/INDUS_util.pyx"],\
-            include_dirs=[numpy.get_include()], \
-            define_macros=[("NPY_NO_DEPRECATED_API", "NPY_1_7_API_VERSION")]),\
-        Extension("analysis_code.Liquid_crystal.GAFF_LC",["analysis_code/Liquid_crystal/GAFF_LC.pyx"],\
-            include_dirs=[numpy.get_include()], \
-            define_macros=[("NPY_NO_DEPRECATED_API", "NPY_1_7_API_VERSION")]),\
-        Extension("analysis_code.droplet.droplet",["analysis_code/droplet/droplet.pyx"],\
-            include_dirs=[numpy.get_include()], \
-            define_macros=[("NPY_NO_DEPRECATED_API", "NPY_1_7_API_VERSION")])]
 
-setup(name='analysis_code',ext_modules = cythonize(extensions,compiler_directives={'language_level' : "3"}))
+setup(name='analysis_code',\
+        version='0.0.1',\
+        author='Yusheng Cai',\
+        packages=find_packages())
